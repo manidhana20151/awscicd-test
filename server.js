28c7c873a17d1f581b4f973188fa075e04ef10e9
manidhana20151 <68317481+manidhana20151@users.noreply.github.com>
2020-09-09 12:52:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS with CodePipeline!');
+  response.end('Hello 1 from Node Server App deployed on ECS with CodePipeline!');
   
 };
 var www = http.createServer(handleRequest);
